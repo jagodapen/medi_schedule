6e59b748b12836935ee8272693ade4f09695b4fa
jagodapen <zlotawannagh@gmail.com>
2023-07-09 20:29:31 +0200

    Add Patient model tests

---
+++ spec/models/patient_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Patient, type: :model do
+  it { should validate_uniqueness_of :pesel_number }
+  it { should validate_presence_of :first_name }
+  it { should validate_presence_of :last_name }
+  it { should validate_presence_of :pesel_number }
+  it { should validate_presence_of :birth_date }
+end
