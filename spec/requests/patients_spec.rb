dcad95f19e220bf69eb1fb3a85fc63817e9d0636
jagodapen <zlotawannagh@gmail.com>
2023-07-11 11:12:14 +0200

    Fix request test

--- spec/requests/patients_spec.rb
+++ spec/requests/patients_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Patients", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/patients/index"
+      get "/patients"
       expect(response).to have_http_status(:success)
     end
   end
